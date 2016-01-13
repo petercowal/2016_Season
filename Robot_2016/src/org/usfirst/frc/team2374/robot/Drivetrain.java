c93ee0bb2037517188c3781de32a6775ac1b5182
2374 <crusaderbots@gmail.com>
2016-01-13 15:13:20 -0800

    Finish solenoid method for front implementation.

--- Robot_2016/src/org/usfirst/frc/team2374/robot/Drivetrain.java
+++ Robot_2016/src/org/usfirst/frc/team2374/robot/Drivetrain.java
@@ -5,54 +5,67 @@ import edu.wpi.first.wpilibj.Talon;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 
-
-
 public class Drivetrain {
-	Talon l1, l2, r1, r2;//talons, pretty normal
+	Talon l1, l2, r1, r2;// talons, pretty normal
 	Encoder encoder;
-	DoubleSolenoid solenoidBack, solenoidFront; //two solenoids, in the future specify or modify these names based on the placement on the robot, for specification's sake
+	DoubleSolenoid solenoidBack, solenoidFront; // two solenoids, in the future
+												// specify or modify these names
+												// based on the placement on the
+												// robot, for specification's
+												// sake
 	int state;
 	int counter;
-	
-	double targetHeading, targetDistance; //used for automatic movement
-	
-	public Drivetrain(){
-		l1=new Talon(0);//ports, to be changed later
-		l2=new Talon(1);
-		r1=new Talon(2);
-		r2=new Talon(3);	
-		encoder=new Encoder(2,3);	//Change ports later if necessary
-		targetHeading=0;
-		solenoidBack=new DoubleSolenoid(1,2); //CHANGE PORTS LATER
-		solenoidFront=new DoubleSolenoid(2,3); //CHANGE THE PORTS
+
+	double targetHeading, targetDistance; // used for automatic movement
+
+	public Drivetrain() {
+		l1 = new Talon(0);// ports, to be changed later
+		l2 = new Talon(1);
+		r1 = new Talon(2);
+		r2 = new Talon(3);
+		encoder = new Encoder(2, 3); // Change ports later if necessary
+		targetHeading = 0;
+		solenoidBack = new DoubleSolenoid(1, 2); // CHANGE PORTS LATER
+		solenoidFront = new DoubleSolenoid(2, 3); // CHANGE THE PORTS
 	}
-	
-	boolean backSolCanPress=true;
-	
-	public void update(double leftSpeed, double rightSpeed, boolean backSolButtonPressed, boolean frontSolButtonPressed){ //perhaps revise the names in the future for the sake of succinctness
+
+	boolean backSolCanPress = true;
+	boolean frontSolCanPress = true;
+
+	public void update(double leftSpeed, double rightSpeed,
+			boolean backSolButtonPressed, boolean frontSolButtonPressed) { // perhaps revise the names in the future for the sake of succinctness
+
 		l1.set(leftSpeed);
 		l2.set(leftSpeed);
 		r1.set(rightSpeed);
 		r2.set(rightSpeed);
-		if(backSolButtonPressed){ //REVISE THIS METHOD PLEASE
-			if(backSolCanPress){
-				if(solenoidBack.get()==DoubleSolenoid.Value.kOff){
+		if (backSolButtonPressed) { // REVISE THIS METHOD PLEASE
+			if (backSolCanPress) {
+				if (solenoidBack.get() == DoubleSolenoid.Value.kOff) {
 					solenoidBack.set(DoubleSolenoid.Value.kForward);
-				}
-				else if(solenoidBack.get()==DoubleSolenoid.Value.kForward){
+				} else if (solenoidBack.get() == DoubleSolenoid.Value.kForward) {
 					solenoidBack.set(DoubleSolenoid.Value.kReverse);
-				}
-				else if(solenoidBack.get()==DoubleSolenoid.Value.kReverse){
+				} else if (solenoidBack.get() == DoubleSolenoid.Value.kReverse) {
 					solenoidBack.set(DoubleSolenoid.Value.kOff);
 				}
-				backSolCanPress=false;
-			}
-			else{
-				backSolCanPress=true;
+				backSolCanPress = false;
+			} else {
+				backSolCanPress = true;
 			}
 		}
-		if(frontSolButtonPressed){
-			
+		if (frontSolButtonPressed) {
+			if (solenoidFront.get() == DoubleSolenoid.Value.kOff) {
+				solenoidFront.set(DoubleSolenoid.Value.kForward);
+			}
+			if (solenoidFront.get() == DoubleSolenoid.Value.kForward) {
+				solenoidFront.set(DoubleSolenoid.Value.kReverse);
+			}
+			if (solenoidFront.get() == DoubleSolenoid.Value.kReverse) {
+				solenoidFront.set(DoubleSolenoid.Value.kOff);
+			}
+			frontSolCanPress = false;
+		} else {
+			frontSolCanPress = true;
 		}
 	}
 }
