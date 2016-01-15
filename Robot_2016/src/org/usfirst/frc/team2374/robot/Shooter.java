0941d6ca70b23c5c1648fd4ea2c08d95dcb41a15
Crusaderbots <crusaderbots@gmail.com>
2016-01-15 15:43:33 -0800

    Add PID control to be further revised with mechanical information.

--- Robot_2016/src/org/usfirst/frc/team2374/robot/Shooter.java
+++ Robot_2016/src/org/usfirst/frc/team2374/robot/Shooter.java
@@ -6,66 +6,95 @@ import edu.wpi.first.wpilibj.Talon;
 
 public class Shooter {
 	Talon wheel;
-	Encoder wheelEncoder; 
+	Encoder wheelEncoder;
 
-	public Shooter(int wheelPort, int encoderPort1, int encoderPort2) { //Remember to add up-down functionality with the wheel!
+	public Shooter(int wheelPort, int encoderPort1, int encoderPort2) { // Remember
+																		// to
+																		// add
+																		// up-down
+																		// functionality
+																		// with
+																		// the
+																		// wheel!
 		wheel = new Talon(wheelPort);
 		wheelEncoder = new Encoder(encoderPort1, encoderPort2, true,
-				EncodingType.k4X); 	// Please look into how encoders really work
+				EncodingType.k4X); // Please look into how encoders really work
 									// in the context of the wheel
-									// and which ports they should use with respect to the wheel;
-									// also look into the constructor for the encoder and encoding
+									// and which ports they should use with
+									// respect to the wheel;
+									// also look into the constructor for the
+									// encoder and encoding
 									// types and all that jazz
 
 	}
 
 	boolean wheelForwardEngaged = false;
 	boolean wheelReverseEngaged = false;
-	public void update(double wheelSpeed, boolean forwardWheelButtonPressed, boolean reverseWheelButtonPressed) { //Comment through this method for review and/or clarity
+
+	public void update(double wheelSpeed, boolean forwardWheelButtonPressed,
+			boolean reverseWheelButtonPressed) { // Comment through this method
+													// for review and/or clarity
 		if (forwardWheelButtonPressed) {
-			wheelForwardEngaged=!wheelForwardEngaged;
+			wheelForwardEngaged = !wheelForwardEngaged;
 		}
-		if(wheelForwardEngaged){
+		if (wheelForwardEngaged) {
 			wheel.set(wheelSpeed);
 		}
-		if(!wheelForwardEngaged){
+		if (!wheelForwardEngaged) {
 			wheel.set(0);
 		}
 		if (reverseWheelButtonPressed) {
-			wheelReverseEngaged=!wheelReverseEngaged;
+			wheelReverseEngaged = !wheelReverseEngaged;
 		}
-		if(wheelReverseEngaged){
+		if (wheelReverseEngaged) {
 			wheel.set(-wheelSpeed);
 		}
-		if(!wheelReverseEngaged){
+		if (!wheelReverseEngaged) {
 			wheel.set(0);
 		}
-		if(wheelForwardEngaged&&wheelReverseEngaged){
+		if (wheelForwardEngaged && wheelReverseEngaged) {
 			wheel.set(-wheelSpeed);
-			wheelForwardEngaged=false;
+			wheelForwardEngaged = false;
 		}
 	}
-	
-	public double getRate(){
+
+	public double getRate() {
 		return wheelEncoder.getRate();
 	}
-	
-	public double Integrate(double voltage){
+
+	double prevError;
+
+	public double Integrate(double voltage) {
 		double integral = 0;
-		
+
 		return integral;
 	}
-		
-	public void PID(double target, double voltage) {
-		double error=getRate()-target;
-		double P=0.5;
-		double I=0.5;
-		double motorVoltage = voltage;//how do we actually find out the motor voltage?
-		double integral=Integrate(motorVoltage);
-		if(Math.abs(error)>=Math.random()*100){ //WE DEFINITELY NEED TO CHANGE THAT
-			motorVoltage=motorVoltage+(error*P);
-			
+
+	double motorVoltage = 0;
+	double integral;
+
+	public void PID(double target) {
+		double error = getRate() - target;
+		double P = 0.005;
+		double I = 0.00005;
+		double D = -0.005;
+		if (Math.abs(error) >= 0) { // WE DEFINITELY NEED TO CHANGE THAT
+			motorVoltage += (error * P) + (error - prevError) * D
+					+ (integral * I);
+			if (motorVoltage > 1) {
+				motorVoltage = 1;
+			} else if (motorVoltage < -1) {
+				motorVoltage = -1;
+			}
+			wheel.set(motorVoltage);
+			integral += error;
+			prevError = error;
 		}
-		
+	}
+	
+	public void resetPid(){
+		prevError = 0;
+		integral = 0;
+		motorVoltage = 0;
 	}
 }
